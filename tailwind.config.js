ddf74c9d542017fcb0ad4e6987705ccd2a77af03
J.Kanishkha <56876652+Jkanishkha0305@users.noreply.github.com>
2024-09-19 18:59:15 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/3d-portfolio/src/assets/herobg.png')",
       },
     },
   },
